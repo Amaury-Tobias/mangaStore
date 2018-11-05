fbf50a8fe93a6c648630ff5b41fe33b24703a4f3
Amaury-Tobias@CSI <amaury.tobiasqr+csi@gmail.com>
2018-11-05 10:36:17 -0600

    properties update

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -54,8 +54,9 @@ public class Main {
         }
     }
 */
-    //@Value("${spring.datasource.url}")
-    private String dbUrl = "jdbc:mysql://fhjylm9br7q9zkvv:umhf0dyxyrgxjre3@x3ztd854gaa7on6s.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/u9awjg2n3b4l5ogo";
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
     @Bean
     public DataSource dataSource() throws SQLException {
         if (dbUrl == null || dbUrl.isEmpty()) {
